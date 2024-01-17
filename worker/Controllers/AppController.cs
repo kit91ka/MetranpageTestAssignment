a288770e97825b2d73e256a3eab897f3a600c1f3
kit91ka <kit91ka@gmail.com>
2024-01-17 18:27:10 +0100

    убрал ф-ю для проверки

--- worker/Controllers/AppController.cs
+++ worker/Controllers/AppController.cs
@@ -32,11 +32,5 @@ namespace worker.Controllers
       return Ok(new { success = true, buildedProject = $"Project ID {requestData.id} Template args: "
         + $"arg1 = {requestData.template.arg1}, arg2 = arg1-2 {requestData.template.arg2}" });
     }
-    [HttpGet("qq")]
-    public async Task<IActionResult> qq()
-    {
-      await Task.CompletedTask;
-      return Ok(new { success = true, buildedProject = "ok"});
-    }
   }
 }
